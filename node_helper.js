4e645ffc2bd2e4891ba257cbcc7692efd3435cc2
J.Gocke <johannes_gocke@hotmail.de>
2021-09-01 00:01:46 +0200

    Revert "Repleace request library with node-fetch"

--- node_helper.js
+++ node_helper.js
@@ -1,15 +1,17 @@
 var NodeHelper = require('node_helper');
+var request = require('request');
 var moment = require('moment');
-const fetch = require('node-fetch');
 
-module.exports = NodeHelper.create( {
+module.exports = NodeHelper.create(
+    {
         // Gets automatically called when the module starts
         start: function () {
             console.log("MagicMirror-FootballLeagues Started...");
         },
 
         // Starts all data gathering background processes
-        startGatherData: function (leagues, showLogos, showTables, apiKey, id, showUnavailable) {
+        startGatherData: function (leagues, showLogos, showTables, apiKey, id, showUnavailable) 
+        {
             // First, gathers all available league ids, gracefully handles if a league doesn't exist
             var self = this;
 
@@ -18,77 +20,92 @@ module.exports = NodeHelper.create( {
             console.log("Refresh every: " + refreshTime + "ms.")
 
             //Check whether unavailable leagues should be shown.
-            if (showUnavailable) {
-                for (var j = 0; j < leagues.length; j++) {
+            if (showUnavailable)
+            {
+                for (var j = 0; j < leagues.length; j++) 
+                {
                     var league = leagues[j];
-                    
+
                     var options = {
                         method: 'GET',
+                        url: 'http://api.football-data.org/v2/competitions/' + league.toString(),
                         headers: {
                             'X-Auth-Token': apiKey
-                    }
+                        }
+                    };
 
-                    fetch('http://api.football-data.org/v2/competitions/' + league.toString(), {
-                          options
-                    }).then(function (error, response, body) {
+                    request(options,
+                        function (error, response, body)
+                        {
                             var competition = JSON.parse(body);
 
                             // Second, start gathering background processes for available league 
                             self.getMatches(league, apiKey, refreshTime, id);
 
-                            if (showTables) {
+                            if (showTables) 
+                            {
                                 self.getTable(league, apiKey, refreshTime, id);
                             }
 
-                            if (showLogos) {
+                            if (showLogos) 
+                            {
                                 self.getTeamLogos(league, apiKey, id);
                             }
 
                             var cap = ""
-                            if (competition.hasOwnProperty('name')) {
+                            if (competition.hasOwnProperty('name'))
+                            {
                                 cap = competition.name
                             }
 
                             // Third, send notification that leagues exist
-                            self.sendSocketNotification('LEAGUES' + id, {
+                            self.sendSocketNotification('LEAGUES' + id,
+                                {
                                     name: cap,
                                     id: league
-                            });
-                    });
+                                });
+                        });
                 }
             }
             else
             {
                 var options = {
                     method: 'GET',
+                    url: 'http://api.football-data.org/v2/competitions',
                     headers: {
                         'X-Auth-Token': apiKey
                     }
                 };
 
-                // Fetch all available leagues
-                fetch('http://api.football-data.org/v2/competitions', {
-                    options
-                }).then(function (error, response, body) {
+                // Request all available leagues
+                request(options,
+                    function (error, response, body)
+                    {
                         var competitions = JSON.parse(body).competitions;
 
-                        for (var i = 0; i < leagues.length; i++) {
-                            for (var j = 0; j < competitions.length; j++) {
+                        for (var i = 0; i < leagues.length; i++) 
+                        {
+                            for (var j = 0; j < competitions.length; j++) 
+                            {
                                 // Check if current league fits a competition, if it does, the league is available
-                                if (competitions[j].id === leagues[i]) {
+                                if (competitions[j].id === leagues[i]) 
+                                {
                                     // Second, start gathering background processes for available league  
                                     self.getMatches(competitions[j].id, apiKey, refreshTime, id);
 
-                                    if (showTables) {
+                                    if (showTables) 
+                                    {
                                         self.getTable(competitions[j].id, apiKey, refreshTime, id);
                                     }
 
-                                    if (showLogos) {
+                                    if (showLogos) 
+                                    {
                                         self.getTeamLogos(competitions[j].id, apiKey, id);
                                     }
 
                                     // Third, send notification that leagues exist
-                                    self.sendSocketNotification('LEAGUES' + id, {
+                                    self.sendSocketNotification('LEAGUES' + id,
+                                        {
                                             name: competitions[j].name,
                                             id: competitions[j].id
                                         });
@@ -109,18 +126,19 @@ module.exports = NodeHelper.create( {
             var end = moment().endOf('isoWeek').format('YYYY-MM-DD');
             var options = {
                 method: 'GET',
+                url: 'http://api.football-data.org/v2/competitions/' + leagueId.toString() + '/matches?dateFrom=' + begin + '&dateTo=' + end,
                 headers: {
                     'X-Auth-Token': apiKey
-            }
+                }
+            }; 
 
-            fetch('http://api.football-data.org/v2/competitions/' + leagueId.toString() + '/matches?dateFrom=' + begin + '&dateTo=' + end, {
-                    options
-            }).then(function (error, response, body) {
+            request(options, function (error, response, body) {
                 try {	
                     var data = JSON.parse(body);
                     var fix = data.matches;
                 }
-                catch (e) {
+                catch (e)
+                {
                     console.log("Error with matches from: " + leagueId);
                 }              
                 
@@ -135,19 +153,21 @@ module.exports = NodeHelper.create( {
         },
 
         // Constantly asks for LeagueTables and sends notifications once they arrive
-        getTable: function (leagueId, apiKey, refreshTime, id) {
+        getTable: function (leagueId, apiKey, refreshTime, id)
+        {
             var self = this;
             var options = {
                 method: 'GET',
+                url: 'http://api.football-data.org/v2/competitions/' + leagueId.toString() + '/standings',
                 headers: {
                     'X-Auth-Token': apiKey
                 }
             };
 
-            fetch('http://api.football-data.org/v2/competitions/' + leagueId.toString() + '/standings', {
-                    options
-            }).then(function (error, response, body) {
-                try {
+            request(options, function (error, response, body) 
+            {
+                try
+                {
                     var data = JSON.parse(body);                  
 
                     if (data.hasOwnProperty('standing'))
@@ -155,11 +175,13 @@ module.exports = NodeHelper.create( {
                     else
                         var tab = data.standings;
                 }
-                catch (e) {
+                catch (e)
+                {
                     console.log("Error with table: " + leagueId);
                 }  
 
-                self.sendSocketNotification('TABLE' + id, {
+                self.sendSocketNotification('TABLE' + id,
+                {
                     leagueId: leagueId,
                     table: tab
                 });
@@ -172,41 +194,49 @@ module.exports = NodeHelper.create( {
         },
 
         // Aquires TeamLogos
-        getTeamLogos: function (leagueId, apiKey, id) {
+        getTeamLogos: function (leagueId, apiKey, id)
+        {
             var self = this;
             var options = {
                 method: 'GET',
+                url: 'http://api.football-data.org/v2/competitions/' + leagueId.toString() + '/teams',
                 headers: {
                     'X-Auth-Token': apiKey
                 }
             };
-            
-            fetch('http://api.football-data.org/v2/competitions/' + leagueId.toString() + '/teams', {
-                    options
-            }).then(function (error, response, body) {
-                try {
+          
+            request(options, function (error, response, body)
+            {
+                try
+                {
                     var teamLogos = {};
 
                     var data = JSON.parse(body);
-                    for (var i = 0; i < data.teams.length; i++) {
-                        try {
+                    for (var i = 0; i < data.teams.length; i++)
+                    {
+                        try
+                        {
                             teamLogos[data.teams[i].id] = data.teams[i].crestUrl;
                         }
-                        catch (e) {
+                        catch (e)
+                        {
                             console.log("Error with logo: " + data.teams[i].id + " - " + data.teams[i].name);
                         } 
                     }
                 }
-                catch (e) {
+                catch (e)
+                {
                     console.log("Error with logos: " + leagueId);
                 } 
 
-                self.sendSocketNotification('LOGO' + id, {
+                self.sendSocketNotification('LOGO' + id,
+                    {
                         leagueId: leagueId,
                         table: teamLogos
                     });
                 /*
-                for (var i = 0; i < data.teams.length; i++) {
+                for (var i = 0; i < data.teams.length; i++)
+                {
                     var idString = data.teams[i]._links.self.href;
                     idString = idString.substring(idString.lastIndexOf("/") + 1, idString.length);
                     teamIds.push(idString);
@@ -216,7 +246,8 @@ module.exports = NodeHelper.create( {
 
         // Receives startup notification, containing config data
         socketNotificationReceived: function (notification, payload) {
-            if (notification === 'CONFIG') {
+            if (notification === 'CONFIG')
+            {
                 this.startGatherData(payload.leagues, payload.showLogos, payload.showTables, payload.apiKey, payload.id, payload.showUnavailable);           
             }
         }
